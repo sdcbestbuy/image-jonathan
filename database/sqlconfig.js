6c158d62d44b8d789903e224caa9e5594031e18b
Christopher Murray <cmurray1105@gmail.com>
2020-06-26 11:01:14 -0500

    commit

--- database/sqlconfig.js
+++ database/sqlconfig.js
@@ -3,5 +3,5 @@ module.exports = {
   user     : process.env.RDS_USERNAME||'root',
   password : process.env.RDS_PASSWORD||'Bruins2011!',
   port     : process.env.RDS_PORT||3306,
-  database: process.env.RDS_DB_NAME||'bestbuy'
+  database : process.env.RDS_DB_NAME||'bestbuy',
 };
