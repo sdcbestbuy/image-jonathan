da24cdfde1aafd798131a36f9178c098c6212f5b
Christopher Murray <cmurray1105@gmail.com>
2020-06-26 12:07:07 -0500

    commit

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,10 +1,6 @@
 import App from './app.jsx';
 import ReactDOM from 'react-dom';
 import React from 'react'
-const Test = () =>{
-return (
-<h1>Hello World</h1>
-)
-}
 
-ReactDOM.render(<Test />, document.getElementById('app'));
+
+ReactDOM.render(<App />, document.getElementById('app'));
