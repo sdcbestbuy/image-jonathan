672c465f569c136aa1f67985d3a6c4774ee7e21f
Christopher Murray <cmurray1105@gmail.com>
2020-07-01 12:03:21 -0500

    added event listener to change current product

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -9,59 +9,66 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-    dataLoaded: false,
-    customerLocation: false,
-    currentProduct: 5,
-    productInfo: [],
-    customerLocation: []
+      dataLoaded: false,
+      customerLocation: false,
+      currentProduct: 5,
+      productInfo: [],
+      customerLocation: []
+
 
     }
-      //bind event handler
-  this.getAllData= this.getAllData.bind(this);
-  this.getZip=this.getZip.bind(this);
+    //bind event handler
+    this.getAllData = this.getAllData.bind(this);
+    this.getZip = this.getZip.bind(this);
   };
-    componentDidMount(){
-      this.getAllData(this.state.productInfo);
-      this.getZip();
-    //   window.addEventListener('click', (event)=> {
-    //     if (event.target.getAttribute('data-id') !== NaN){
-    //     this.setState({currentProduct: event.target.getAttribute('data-id')})
-    //     this.getAllData(this.state.productInfo)
-    // })
+  componentDidMount() {
+    this.getAllData(this.state.productInfo);
+    this.getZip();
+    window.addEventListener('click', (event) => {
+      console.log(event.view.id)
+      if (event.view.id !== undefined) {
+        if (event.view.id !== this.state.currentProduct) {
+          this.setState({ currentProduct: event.view.id })
+          this.getAllData(this.state.productInfo)
+        }
+      }
+    })
   }
 
 
   //event handlers
-getAllData() {
-  axios.get('/display')
-  .then(( data )=>{
-    this.setState({productInfo: data,
-    dataLoaded: true})
-    console.log(this.state.productInfo.data[1])
-  })
-  .catch( (error)=> {
-    console.log(error);
-  });
-};
+  getAllData() {
+    axios.get('/display')
+      .then((data) => {
+        this.setState({
+          productInfo: data,
+          dataLoaded: true
+        })
+        console.log(this.state.productInfo.data[1])
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
 
-getZip(){
-  $.getJSON('http://ip-api.com/json?callback=?', (data)=> {
-    console.log("data", data)
-  this.setState({customerLocation: data, customerLoaded: true});
-  })
-  .fail(()=>{
-    console.log('error')
-  })
-}
+  getZip() {
+    $.getJSON('http://ip-api.com/json?callback=?', (data) => {
+      console.log("data", data)
+      this.setState({ customerLocation: data, customerLoaded: true });
+    })
+      .fail(() => {
+        console.log('error')
+      })
+  }
 
   //render
-  render(){
+  render() {
     return (
       <div>
-        {(this.state.dataLoaded && this.state.customerLoaded) ? <ProductView customerLocation={this.state.customerLocation} productInfo={this.state.productInfo.data[this.state.currentProduct-1]}/> : 'Loading...'}
-          <div className="side"></div>
-        </div>
+        {(this.state.dataLoaded && this.state.customerLoaded) ? <ProductView customerLocation={this.state.customerLocation} productInfo={this.state.productInfo.data[this.state.currentProduct - 1]} /> : 'Loading...'}
+        <div className="side"></div>
+      </div>
 
     )
   }
