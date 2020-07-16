715bd300d238b307550528df4b50420f920de36f
Jonathan Zurita <62966232+JonathanZurita@users.noreply.github.com>
2020-07-16 17:30:44 -0500

    update knexfile

--- knexfile.js
+++ knexfile.js
@@ -5,10 +5,10 @@ module.exports = {
     development: {
         client: "pg", 
         connection: {//specify your particular client here
-            user: `${process.env.DB_USER}`,
-            host: `${process.env.DB_HOST}`,
-            database: `${process.env.DB_DBNAME}`,
-            password: `${process.env.DB_PASS}`
+            user: `postgres`,
+            host: `34.229.224.145`,
+            database: `test`,
+            password: `password`
         }, //use specific database connection here
         migrations: {
             directory: __dirname + "/migrations"
