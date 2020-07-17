0836005aa8ed75f1e0a4273f43eb2937086db39c
Jonathan Zurita <62966232+JonathanZurita@users.noreply.github.com>
2020-07-16 21:56:34 -0500

    update stages to k6 test

--- stages.js
+++ stages.js
@@ -8,12 +8,12 @@ export let options = {
     stages: [
       { duration: '1m', target: 1 }, // below normal load
       { duration: '1m', target: 10 },
-      { duration: '1m', target: 100 }, // spike to 1400 users
-      { duration: '1m', target: 200 }, // stay at 1400 for 3 minutes
-      { duration: '2m', target: 230 },
-      { duration: '2m', target: 200 },
+      { duration: '1m', target: 50 }, // spike to 1400 users
+      { duration: '1m', target: 80 }, // stay at 1400 for 3 minutes
+      { duration: '2m', target: 100 },
+      { duration: '2m', target: 40 },
       { duration: '10s', target: 5 }, // scale down. Recovery stage.
-      { duration: '1m', target: 50 }, 
+      { duration: '1m', target: 20 }, 
       { duration: '10s', target: 0 },
     ],
 };
